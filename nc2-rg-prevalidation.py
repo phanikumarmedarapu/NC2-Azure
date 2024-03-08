31040dc944b8157651ace2b4f90365ebdfe65bf7
beyondnotice <andrew.nam@nutanix.com>
2024-03-08 13:41:23 +1100

    Update nc2-rg-prevalidation.py
    minor improvement including format and delegation function to include subnet info

--- nc2-rg-prevalidation.py
+++ nc2-rg-prevalidation.py
@@ -1,11 +1,11 @@
-# Author : Andrew Nam     andrew.nam@nutanix.com
-# Date : 2024Mar07
-# source : natgw_publicip_tag_RG_all_4.sh
-# Goal : convert natgw_publicip_tag_RG_all_4.sh to python
+# Date : 2024Mar08
+# source : natgw_publicip_tag_RG_all_4-3.sh
+# Goal : convert natgw_publicip_tag_RG_all_4-2.sh to python
 # fixed :
 # 1. nat gateway per vnet
 # 2. delegation info included
 # 3. dns per vnet
+# 4. minor format improvement made
 
 import subprocess
 import json
@@ -17,21 +17,27 @@ def get_subnet_info(rg_name, vnet_name):
 def check_subnet_delegation(rg_name, vnet_name):
     subnet_list = subprocess.run(['az', 'network', 'vnet', 'subnet', 'list', '--resource-group', rg_name, '--vnet-name', vnet_name], capture_output=True, text=True)
     if "Microsoft.Network/virtualNetworks/subnets/delegations" in subnet_list.stdout:
+        print(" ")
+        print(" ")
         print(f"[RESULT] Microsoft.BareMetal.AzureHostedService subnet delegation found for {vnet_name}:")
         #subprocess.run(['az', 'network', 'vnet', 'subnet', 'list', '--resource-group', rg_name, '--vnet-name', vnet_name, '--query', "[?contains(@.delegations, 'Microsoft.Network/virtualNetworks/subnets/delegations')]", '--output', 'json'])
         with open('subnet_delegate.txt', 'w') as file:
             json.dump(json.loads(subnet_list.stdout), file, indent=4)
         print("[RESULT] subnet delegate to :")
-        subprocess.run(['grep', 'Microsoft.Network/virtualNetworks/subnets/delegations', '-B3', 'subnet_delegate.txt'])
+        subprocess.run(['grep', 'Microsoft.Network/virtualNetworks/subnets/delegations', '-B5', 'subnet_delegate.txt'])
         # Microsoft.Network/virtualNetworks/subnets/delegations -B3
         print("")
     else:
+        print(" ")
+        print(" ")
         print(f"[RESULT] No subnet delegation found in {vnet_name} !!!!")
 
 def check_vnet_peering(rg_name, vnet_name):
     output_peer = subprocess.run(['az', 'network', 'vnet', 'peering', 'list', '--resource-group', rg_name, '--vnet-name', vnet_name], capture_output=True, text=True)
     peering_found = any([p for p in json.loads(output_peer.stdout) if 'peeringState' in p and 'type' in p and p['type'] == 'Microsoft.Network/virtualNetworks/virtualNetworkPeerings'])
     if peering_found:
+        print(" ")
+        print(" ")
         print("Entries found with 'peeringState' and 'type' as 'Microsoft.Network/virtualNetworks/virtualNetworkPeerings':")
         with open('vnet_peering.txt', 'w') as file:
             json.dump(json.loads(output_peer.stdout), file, indent=4)
@@ -44,17 +50,20 @@ def check_vnet_peering(rg_name, vnet_name):
 def check_dns_servers(rg_name, vnet_name):
     output_dns = subprocess.run(['az', 'network', 'vnet', 'show', '--resource-group', rg_name, '--name', vnet_name, '--query', 'dhcpOptions.dnsServers'], capture_output=True, text=True)
     if not output_dns.stdout:
-        print("")
+        print(" ")
+        print(" ")
         print("[RESULT] No DNS server IP addresses found.")
         print("")
     else:
-        print("")
+        print(" ")
+        print(" ")
         print("DNS server IP addresses found:")
         print(f"[RESULT] DNS IP : {output_dns.stdout} for {vnet_name}")
         print("")
 
 def check_nsg_association(rg_name, vnet_name):
     subnets = subprocess.run(['az', 'network', 'vnet', 'subnet', 'list', '--resource-group', rg_name, '--vnet-name', vnet_name, '--query', "[].name", '-o', 'tsv'], capture_output=True, text=True).stdout.splitlines()
+    print(" ")
     print(" ")
     print(" ")
     print("NSG Setting")
@@ -76,9 +85,8 @@ def check_nsg_association(rg_name, vnet_name):
 def check_nat_gateway(rg_name, vnet_name):
     nat_gateway_info = subprocess.run(['az', 'network', 'nat', 'gateway', 'list', '--resource-group', rg_name, '--query', "[?provisioningState=='Succeeded'].{Name:name}", '--output', 'json'], capture_output=True, text=True)
     print(" ")
-    #print(nat_gateway_info.stdout)
-    #print(" ")
-    #nat_gateway_found = subprocess.run(['az', 'network', 'vnet', 'subnet', 'list', '--resource-group', rg_name, '--vnet-name', vnet_name, '--query', 'contains(@[].serviceEndpoints[].serviceName, `Microsoft.Network/natGateways`)', '-o', 'json'], capture_output=True, text=True)
+    print(" ")
+    print(" ")
     if not nat_gateway_info.stdout:
         print(f"[RESULT] No NAT Gateway found in VNet '{vnet_name}' of resource group '{rg_name}'.")
         return
